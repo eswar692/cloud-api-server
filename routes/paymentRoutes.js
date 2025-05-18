a6e01f6735dc8555cee503065f0bb69cabb72fc8
eswaraprasad <esweraprasad@gmail.com>
2025-05-18 10:31:39 +0530

    changes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,9 +1,8 @@
 const payment = require("../controllers/paymentController");
-const express= require("express")
+const express = require("express");
 const router = express.Router();
-const { verifyToken } = require("../middlewares/userMiddleware");
+const verifyToken = require("../middlewares/userMiddleware");
 
-router.post("/create-order",verifyToken, payment.paymentOrder);
-
+router.post("/create-order", verifyToken, payment.paymentOrder);
 
 module.exports = router;
