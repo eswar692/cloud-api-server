30cc54a842690fbc9160b1de53edd7b63deebb37
Prasad <digitallogic469@gmail.com>
2025-06-03 20:49:26 +0530

    changes

--- job/define.js
+++ job/define.js
@@ -34,7 +34,9 @@ const agendaDefine = (agenda) => {
       
       // No prepaid plan - expire current plan
       user.plan = "expired";
-      user.freePlanActive = "completed";
+      if(user.freePlanActive === "active") {
+        user.freePlanActive = "completed";
+      }
       user.amount = 0;
       user.endPlanDate = 0;
       user.messageLimit = 0;
@@ -52,9 +54,35 @@ const agendaDefine = (agenda) => {
       await sendOTP(userDetails.email, emailOptionsInstance);
 
       console.log(`Plan expired for user: ${userId}`);
+      // Delete the job in Mongo DB 
        await job.remove();
     } catch (error) {
       console.error(`Error processing plan expiration job: ${error.message}`, error);
+    }
+  });
+  agenda.define("plan-alert", async (job) => {
+    try {
+      const { userId } = job.attrs.data;
+      console.log(`Processing plan alert for user: ${userId}`);
+      
+      const user = await Payment.findOne({ userId });
+      if (!user) {
+        console.log(`User not found with ID: ${userId}`);
+        return;
+      }
+      const userDetails = await User.findById(userId);
+      if(!userDetails) {
+        console.log(`User details not found for ID: ${userId}`);
+        return;
+      }
+      const emailOptionsInstance = mailOptions({title:"Your Plan is about to expire in 2 days",email:userDetails.email, message:{text:"Plan Alert",main:"Please renew your plan",sub:"your plan is about to expire"}, otp:null});
+      await sendOTP(userDetails.email, emailOptionsInstance);
+
+      console.log(`Plan alert sent for user: ${userId}`);
+      // Delete the job in Mongo DB 
+       await job.remove();
+    } catch (error) {
+      console.error(`Error processing plan alert job: ${error.message}`, error);
     }
   });
 };
